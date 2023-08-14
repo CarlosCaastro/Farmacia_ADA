880f57413c46ef13deb6bcac21eb716f146c9c2d
Carlos Castro <carlos.castro@elogroup.com.br>
2023-08-14 12:33:00 -0300

    Algumas verificações no número e ddd

--- Objetos/laboratorio.py
+++ Objetos/laboratorio.py
@@ -10,13 +10,16 @@ class Laboratorio:
         
         if ddd == "00":
             raise ValueError("O DDD não pode ser '00'.")
+
+        if len(telefone) != 9:
+            raise ValueError("O numero de telefone precisa conter nove caracteres. Verifique se não esqueceu de colocar o digito 9 na frente.")
         
         self.nome = nome
         self.endereco = endereco
-        self.ddd = ddd
+        self.ddd = ddd.zfill(3)
         self.telefone = telefone
         self.cidade = cidade
         self.estado = estado
 
     def __repr__(self) -> str:
-        return f'Nome: {self.nome}\nEndereço: {self.endereco}\nTelefone: {self.telefone}\nCidade: {self.cidade}\nEstado: {self.estado}'
+        return f'Nome: {self.nome}\nEndereço: {self.endereco}\nDDD: {self.ddd}\nTelefone: {self.telefone}\nCidade: {self.cidade}\nEstado: {self.estado}'
